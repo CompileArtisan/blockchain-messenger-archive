19cdff1821e5ce85eac92da4597dce707fdb2f92
CompileArtisan <praanesh.b.nair@gmail.com>
2024-05-10 23:19:56 +0530

    refactor: Serialize and deserialize Block objects

--- Block.java
+++ Block.java
@@ -1,8 +1,13 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.security.PublicKey;
 import java.util.HashMap;
 
-public class Block {
+public class Block implements Serializable{
     private HashMap<String, PublicKey> userKeyPairs = new HashMap<>();
     private Message message;
     private String previousHash;
@@ -24,4 +29,26 @@ public class Block {
     public PublicKey getPublicKey(String userName) {
         return userKeyPairs.get(userName);
     }
+
+    // method to serialize the block
+    public void serializeBlock(String fileName) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fileName))) {
+            oos.writeObject(this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    // method to deserialize the block
+    public static Block deserializeBlock(String fileName) {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fileName))) {
+            return (Block) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    
 }
