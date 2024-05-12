b2ac0ad9418a2c487df94b7668e659422435c518
CompileArtisan <praanesh.b.nair@gmail.com>
2024-05-12 15:22:09 +0530

    refactor: Add getHash() method to Block class for generating SHA-256 hash

--- Block.java
+++ Block.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
 import java.util.HashMap;
 
@@ -52,6 +55,27 @@ public class Block implements Serializable{
             e.printStackTrace();
         }
         return null;
+    }
+
+    public String getHash() {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            String data = "Block{" +
+                    "userKeyPairs=" + userKeyPairs +
+                    ", previousHash='" + previousHash +
+                    ", message=" + message +
+                    '}';
+            byte[] hash = digest.digest(data.getBytes(StandardCharsets.UTF_8));
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hash) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        }  catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     
